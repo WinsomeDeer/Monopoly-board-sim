53deb8cef26553c04e49ab11f0a77d50f5037c66
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-04-13 20:02:53 +0100

    Update monopoloy.cpp

--- monopoloy.cpp
+++ monopoloy.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <cmath>
 #include <stdlib.h>
+#include <cstdlib>
 #include <iomanip>
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
+#include <time.h>
 
 // Function to calculate dice throws and keep track of number of doubles.
 int dice_throw(int& no_doubles){
-    int dice1 = (std::rand() % 6), dice2 = (std::rand() % 6);
+    int dice1 = (std::rand() % 6 + 1), dice2 = (std::rand() % 6 + 1);
     if(dice1 == dice2){
         no_doubles++;
         if(no_doubles == 3){
@@ -27,7 +29,7 @@ int dice_throw(int& no_doubles){
 // Jail Function.
 int jail(){
     for(int i = 0; i < 3; i++){
-        int dice1 = (std::rand() % 6), dice2 = (std::rand() % 6);
+        int dice1 = (std::rand() % 6 + 1), dice2 = (std::rand() % 6 + 1);
         if(dice1 == dice2){
             return dice1 + dice2;
         }
@@ -37,7 +39,7 @@ int jail(){
 
 // Community chest function.
 int community_chest(){
-    int card = (rand() % 17) + 1;
+    int card = (rand() % 16) + 1;
     std::unordered_map<int, int> mp = {{1,0}, {5,-1}, {6,10}};
     if(mp.find(card) != mp.end()){
         return mp[card];
@@ -49,7 +51,7 @@ int community_chest(){
 
 // Chance card function.
 int chance_card(int& moves){
-    int card = (rand() % 17) + 1;
+    int card = (rand() % 16) + 1;
     std::unordered_map<int, int> mp
         = {{1,0},{2,24},{3,39},{4,11},{5, std::min({abs(5-moves), abs(15-moves), abs(25-moves), abs(35-moves)})},
             {6,std::min({abs(12-moves), abs(28-moves)})}, {8,-1}, {9, moves-3}, {10, 10}, {13,5}, {14, 0}};
@@ -63,11 +65,21 @@ int chance_card(int& moves){
 
 // Main loop to count the squares landed on.
 int main(){
-    int i = 0, sq_total = 40, moves = 0, no_of_doubles = 0;
+    srand(time(NULL));
+    int i = 0, sq_total = 40, moves = 0, no_of_doubles = 0, move;
     bool GOJ = false, jailed = false;
     std::vector<int> board(sq_total, 0);
     while(i++ <= 10000){
-        moves += dice_throw(no_of_doubles);
+        move = dice_throw(no_of_doubles);
+        // Check if we have rolled three doubles.
+        if(move == -1){
+            moves = 10;
+            jail();
+        }
+        else{
+            moves += move;
+        }
+        board[moves]++;
         // If we've gone past GO.
         if(moves % sq_total <= 40){
             moves = moves % sq_total;
@@ -101,7 +113,6 @@ int main(){
         if(moves == 30){
             board[moves]++;
             if(GOJ){
-
                 GOJ = false;
             }
             else{
@@ -110,6 +121,10 @@ int main(){
             moves = 10;
             board[moves]++;
         }
+    }
+    // For loop to display results in console.
+    for(int i = 0; i < board.size(); i++){
+        std::cout << i << " : " << board[i] << std::endl;
     }
     return 0;
 }
